6966f9a6cb94bc07c74957dafdb4309978daa895
Dreg0 <andrii.kruglakov@gmail.com>
2018-02-13 12:35:56 +0200

    save in changes

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,16 +1,14 @@
-    
-    var range = 5;
-    var attempts = 3;
-    var prize = 10;
-    var currentPrize = 0;
-    var usedAttempts = 0;
-    var randomNumber = Math.floor(Math.random() * (range + 1));
-    console.log("randomized " + randomNumber);
-    var nextLevel = true;
     var play = confirm("Do you want to play a game?");
+    console.log(`randomized  ${randomNumber}`);
 
-
-    if (play) {
+    while (play) {
+        var range = 5;
+        var attempts = 3;
+        var prize = 10;
+        var currentPrize = 0;
+        var usedAttempts = 0;
+        var randomNumber = Math.floor(Math.random() * (range + 1));
+        var nextLevel = true;
         while (true && usedAttempts < 3 && nextLevel) {
             var playerTurn = Number(prompt(`Enter a number from 0 to ${range} \nAttempts left: ${attempts}\nYour Prize: ${currentPrize} $\nPossible prize on current attemp: ${prize}$`));
             if (playerTurn === randomNumber && playerTurn != isNaN) {
@@ -20,19 +18,23 @@
                 usedAttempts = 0;
                 range *= 2;
                 randomNumber = Math.floor(Math.random() * (range + 1));
-                console.log("randomized " + randomNumber);
+                console.log(`randomized  ${randomNumber}`);
                 nextLevel = confirm('Do you want to continue a game?');
-            }
-            else {
-                attempts --;
-                usedAttempts ++;
-                prize = Math.floor (prize / 2 );
+            } else {
+                attempts--;
+                usedAttempts++;
+                prize = Math.floor(prize / 2);
             }
         }
 
         console.log(`Thank you for a game. Your prize is: ${currentPrize}$`);
-        play = confirm("Do you want to play again?");
+        var play = confirm("Do you want to play again?");
+        play++;
+        if (!play) {
+            console.log("You did not become a millionaire");
 
-    } else {
+    }
+}
+    if (!play) {
         console.log("You did not become a millionaire");
     }
