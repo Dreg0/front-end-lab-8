3676b42d066c53b8d060ed2b2127a169c322290f
Dreg0 <andrii.kruglakov@gmail.com>
2018-02-13 13:28:12 +0200

    solution for play again option

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,40 +1,37 @@
-    var play = confirm("Do you want to play a game?");
-    console.log(`randomized  ${randomNumber}`);
-
-    while (play) {
-        var range = 5;
-        var attempts = 3;
-        var prize = 10;
-        var currentPrize = 0;
-        var usedAttempts = 0;
-        var randomNumber = Math.floor(Math.random() * (range + 1));
-        var nextLevel = true;
-        while (true && usedAttempts < 3 && nextLevel) {
-            var playerTurn = Number(prompt(`Enter a number from 0 to ${range} \nAttempts left: ${attempts}\nYour Prize: ${currentPrize} $\nPossible prize on current attemp: ${prize}$`));
-            if (playerTurn === randomNumber && playerTurn != isNaN) {
-                attempts = 3;
-                currentPrize = prize;
-                prize *= 3;
-                usedAttempts = 0;
-                range *= 2;
-                randomNumber = Math.floor(Math.random() * (range + 1));
-                console.log(`randomized  ${randomNumber}`);
-                nextLevel = confirm('Do you want to continue a game?');
-            } else {
-                attempts--;
-                usedAttempts++;
-                prize = Math.floor(prize / 2);
+    window.onload = function () {
+        var play = confirm("Do you want to play a game?");
+        while (play) {
+            var range = 5;
+            var attempts = 3;
+            var prize = 10;
+            var currentPrize = 0;
+            var usedAttempts = 0;
+            var randomNumber = Math.floor(Math.random() * (range + 1));
+            console.log(`randomized  ${randomNumber}`);
+            var nextLevel = true;
+            var playAgain = 0;
+            while (usedAttempts < 3 && nextLevel) {
+                var playerTurn = Number(prompt(`Enter a number from 0 to ${range} \nAttempts left: ${attempts}\nYour Prize: ${currentPrize} $\nPossible prize on current attemp: ${prize}$`));
+                if (playerTurn === randomNumber && playerTurn != isNaN) {
+                    attempts = 3;
+                    currentPrize = prize;
+                    prize *= 3;
+                    usedAttempts = 0;
+                    range *= 2;
+                    randomNumber = Math.floor(Math.random() * (range + 1));
+                    console.log(`randomized  ${randomNumber}`);
+                    nextLevel = confirm('Do you want to continue a game?');
+                } else {
+                    attempts--;
+                    usedAttempts++;
+                    prize = Math.floor(prize / 2);
+                }
             }
+            console.log(`Thank you for a game. Your prize is: ${currentPrize}$`);
+            play = confirm("Do you want to play again?");
+            playAgain++;
         }
-
-        console.log(`Thank you for a game. Your prize is: ${currentPrize}$`);
-        var play = confirm("Do you want to play again?");
-        play++;
-        if (!play) {
+        if (!playAgain) {
             console.log("You did not become a millionaire");
-
-    }
-}
-    if (!play) {
-        console.log("You did not become a millionaire");
-    }
+        }
+    };
