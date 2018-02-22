d228fcd666b01f6d58475c690dfa922426d088ab
Dreg0 <andrii.kruglakov@gmail.com>
2018-02-22 17:46:40 +0200

    finished

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -25,39 +25,96 @@
  * @param {Fighter} fighter - The fighter
  * DO NOT MODIFY
  */
+function fighter(properties) {
 
-/**
- * Your code goes here
- * function foo() { ... }
- */
+    var combatHistory = {
+        wins: 0,
+        loses: 0
+    };
 
-/**
- * The following code must be valid after implementation!
- */ 
+    var stats = {
+        name: properties.name,
+        attack: properties.attack,
+        hp: properties.hp
+    };
 
-// var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
-// var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
-// var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
+    return {
+        getName: function () {
+            return stats.name;
+        },
 
-// fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
-// fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
+        getStats: function () {
+            return stats;
+        },
+
+        getCombatHistory: function () {
+            return combatHistory;
+        },
+
+        block: function () {
+            return Math.random() >= 0.5;
+        },
+
+        fight: function (enemy) {
+            if (enemy.block()) {
+                return false;
+            }
+            var enemyStats = enemy.getStats();
+            var EnemyCombatHistory = enemy.getCombatHistory();
+
+            if (stats.attack >= enemyStats.hp) {
+                enemyStats = 0;
+                combatHistory.wins++;
+                EnemyCombatHistory.loses++;
+            } else {
+                enemyStats.hp -= stats.attack;
+            }
+        }
+    };
+}
+
+function showResult(fighter) {
+    console.log('Fighter', fighter.getName());
+    console.log('- Combat stats:', fighter.getCombatHistory());
+    console.log('- Properties:', fighter.getStats());
+}
+
+
+var fighter1 = fighter({
+    name: 'John',
+    attack: 100,
+    hp: 100
+});
+var fighter2 = fighter({
+    name: 'Kayn',
+    attack: 50,
+    hp: 300
+});
+var fighter3 = fighter({
+    name: 'Bill',
+    attack: 40,
+    hp: 100
+});
+
+fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
+fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
 // /**
 //  * Fighter John
 //  * - Combat stats: { wins: 1, loses: 0 }
 //  * - Properties: { name: 'John', attack: 100, hp: 100 }
 //  */
-// showResult(fighter1);
+showResult(fighter1);
 
 // /** Fighter Kayn
 //  * - Combat stats: { wins: 0, loses: 0 }
 //  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
 //  */
-// showResult(fighter2); 
+showResult(fighter2);
 
 // /**
 //  * Fighter Bill
 //  * - Combat stats: { wins: 0, loses: 1 }
 //  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
 //  */
-// showResult(fighter3);
+showResult(fighter3);
